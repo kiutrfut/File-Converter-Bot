d2fd95d3e4a8a7375c040c2fada3e8e5bddda3ef
kiutrfut <99256842+kiutrfut@users.noreply.github.com>
2023-02-11 03:24:12 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,11 @@
-import asyncio
 from pyrogram import Client
 
-api_id = 22931292
-api_hash = "8a50db8b70f8cbcbc013f013b6a58ac7"
+app = Client("my_account")
 
 
 async def main():
-    async with Client("my_account", api_id, api_hash) as app:
-        await app.send_message("me", "Greetings from **Pyrogram**!")
+    async with app:
+        await app.send_message("me", "Hi!")
 
 
-asyncio.run(main())
+app.run(main())
