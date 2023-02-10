4e7f1adf4f48dd401059f2738b5a79a2cebdb1c1
kiutrfut <99256842+kiutrfut@users.noreply.github.com>
2023-02-11 03:06:26 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,12 @@ from pyrogram import Client
 
 api_id = 22931292
 api_hash = "8a50db8b70f8cbcbc013f013b6a58ac7"
+bot_token = "5298903268:AAEAIN0bVGGItFdSvzHkShJ1_GLEkTdecF8"
 
-app = Client("my_account", api_id=api_id, api_hash=api_hash)
+app = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
 
 app.run()
