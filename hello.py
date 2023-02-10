29dd3d0da61a18811ca9edfcd3ddef17f12f856d
kiutrfut <99256842+kiutrfut@users.noreply.github.com>
2023-02-11 03:26:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 from pyrogram import Client
 
-app = Client("my_account")
+api_id = 22931292
+api_hash = "8a50db8b70f8cbcbc013f013b6a58ac7"
+bot_token = "5298903268:AAEAIN0bVGGItFdSvzHkShJ1_GLEkTdecF8"
 
+app = Client(
+    "my_bot",
+    api_id=api_id, api_hash=api_hash,
+    bot_token=bot_token
+)
 
-async def main():
-    async with app:
-        await app.send_message("me", "Hi!")
-
-
-app.run(main())
+app.run()
